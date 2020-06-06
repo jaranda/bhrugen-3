7c80b80b09ed16bc41197e71f4d00ebcc68e32f9
Juan Alberto Aranda Alvarez <jaranda@a3nstudio.com>
2020-06-06 15:34:42 -0500

    SparkAuto Project
    Adding to the repository.

---
+++ SparkAuto/SparkAuto/Pages/ServiceTypes/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]  // On Post() if this object is found it will automatically pass it and assign values to your Post handlers.
+        public ServiceType ServiceType { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.ServiceType.Add(ServiceType);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+    }
+}
